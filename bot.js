9421e9fc417c832f91b0fae9973109c527394b17
islamsray <67166625+islamsray@users.noreply.github.com>
2020-06-23 14:37:21 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,129 +14,7 @@ client.on("message", message => {
  message.delete(); 
 };     
 });
-client.on("message", message => {
-  if (message.content === "m") {
-    const bot = new Discord.RichEmbed()
-      .setAuthor(client.user.username, client.user.avatarURL)
-      .setColor("#00000")
-      .addField(
-        "✽ **Bot Ping** : ",
-        `» ${Date.now() - client.createdTimestamp}` + " ms",
-        true
-      )
-      .addField("**Servers** :  ", `» ${client.guilds.size}`, true)
-      .addField("**Channels** : ", `» ${client.channels.size} `, true)
-      .addField("**Users** : ", `» ${client.users.size} `, true)
-      .addField("**Bot Name** :  ", `» ${client.user.tag} `, true)
-      .addField("**Bot Owner** :  ", `» <@503612821356019714>`, true) // تعديل اساسي غير الايدي لايدي حسابك
-      .setImage("")
-      .setFooter(message.author.username, message.client.avatarURL);
-    message.channel.send(bot);
-  }
-});
 
-
-
-client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-client.on("message", message => {
-  if (message.content === "السلام عليكم") {
-    message.channel.send("**:heart:وعليكم السلام ورحمة الله وبركاته:heart:**");
-    message.channel.sendFile("");
-  }
-});
-
-client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-client.on("message", message => {
-  if (message.content === "السلام عليكم") {
-    message.channel.send("**:heart:وعليكم السلام ورحمة الله وبركاته:heart:**");
-    message.channel.sendFile("");
-  }
-});
-
-client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-client.on("message", message => {
-  if (message.content === "سلام عليكم") {
-    message.channel.send("**:heart:وعليكم السلام ورحمة الله وبركاته:heart:**");
-    message.channel.sendFile("");
-  }
-});
-
-client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-client.on("message", message => {
-  if (message.content === "سلام") {
-    message.channel.send("**:heart:وعليكم السلام ورحمة الله وبركاته:heart:**");
-    message.channel.sendFile("");
-  }
-});
-
-
-client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-client.on("message", message => {
-  if (message.content === "Virus") {
-    message.channel.send("فيروس يحيكك ياغالي منور ام السيرفر");
-    message.channel.sendFile("");
-  }
-});
-
-client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-client.on("message", message => {
-  if (message.content === "فايروس") {
-    message.channel.send("فيروس يحيكك ياغالي منور ام السيرفر");
-    message.channel.sendFile("");
-  }
-});
-
-client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-client.on("message", message => {
-  if (message.content === "فيروس") {
-    message.channel.send("فيروس يحيكك ياغالي منور ام السيرفر");
-    message.channel.sendFile("");
-  }
-});
-
-client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-client.on("message", message => {
-  if (message.content === "ابغا بوسة") {
-    message.channel.send("مواااااااححححح :heart: ");
-    message.channel.sendFile("");
-  }
-});
-
-client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-client.on("message", message => {
-  if (message.content === "h") {
-    message.channel.send("(b)لارسال رسالة جماعية لجميع أعضاء السيرفر");message.channel.send("(m)لمعرفة معلومات البوت والسيرفر");message.channel.send("(i)لدعوة البوت الى سيرفرك");
-    message.channel.sendFile("");
-  }
-});
-client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-client.on("message", message => {
-  if (message.content === "i") {
-    message.channel.send("رابط دعوة البوت :");
-    message.channel.send("https://discord.com/api/oauth2/authorize?client_id=722169839866740797&permissions=1611823697&scope=bot");
-    message.channel.sendFile("");
-  }
-});
 client.on('ready', () => {
    console.log(`----------------`);
       console.log(`Desert Bot- Script By : darkxx`);
@@ -144,8 +22,6 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : black ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`By Dù_Virus`,"http://twitch.tv/Death Shop")
-client.user.setStatus("dnd")
 });
 
 client.login(process.env.BOT_TOKEN);
